206f508f25e04a2894677f2a0aa81de56228243c
MBURU <wawerumburu157@gmail.com>
2024-10-14 23:16:00 +0300

    Functions

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(callback) {
+    callback(); 
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("I am a named function");
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("I am an anonymous function");
+    };
+}
+
+
+SSSS
